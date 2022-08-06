fd148c25cbc501cdb1df075db79008964aa2f707
emiliosp <emilio.spatola@gmail.com>
2022-08-06 19:22:47 +0200

    add json_find timer

--- index.js
+++ index.js
@@ -3,5 +3,10 @@ import { readFileSync } from 'node:fs';
 const dataString = readFileSync('./large-file.json').toString();
 
 console.time('json_parse_perf');
-JSON.parse(dataString);
+const data = JSON.parse(dataString);
 console.timeEnd('json_parse_perf');
+
+// data is an array of 11351 entries
+console.time('json_find');
+const lastEntry = data.find(d => d.id === '2489678844');
+console.timeEnd('json_find');
