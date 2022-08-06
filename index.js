78e4c1d34ed7905513e9f4fc29ade7c7b10fa7a9
emiliosp <emilio.spatola@gmail.com>
2022-08-06 19:26:46 +0200

    chore

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ console.time('json_parse_perf');
 const data = JSON.parse(dataString);
 console.timeEnd('json_parse_perf');
 
-// data is an array of 11351 entries
+// data is an array of 11351 items
 console.time('json_find');
 const lastEntry = data.find(d => d.id === '2489678844');
 console.timeEnd('json_find');
